59b06bb7d8be9f82b97513b74a61ca06f9666aec
coldTOUFU <coldtoufu.dev@gmail.com>
2022-11-17 20:15:20 +0900

    add: cardとsubmissionのテスト追加; 未実行。

---
+++ test/card_test.cpp
@@ -0,0 +1,145 @@
+#include <gtest/gtest.h>
+
+#include "../src/card.hpp"
+
+TEST(GetColorTest, NormalColor) {
+  Card card1{Color::kBlue, CardNumber::kZero};
+  EXPECT_EQ(card1.getColor(), Color::kBlue);
+
+  Card card2{Color::kGreen, CardNumber::kZero};
+  EXPECT_EQ(card2.getColor(), Color::kGreen);
+
+  Card card3{Color::kRed, CardNumber::kZero};
+  EXPECT_EQ(card3.getColor(), Color::kRed);
+
+  Card card4{Color::kYellow, CardNumber::kZero};
+  EXPECT_EQ(card4.getColor(), Color::kYellow);
+}
+
+TEST(GetColorTest, Null) {
+  Card card{};
+  EXPECT_EQ(card.getColor(), Color::kNull);
+}
+
+TEST(GetColorTest, Wild) {
+  Card card{Color::kWild, CardAction::kWild};
+  EXPECT_EQ(card.getColor(), Color::kWild);
+}
+
+TEST(GetPatternTest, Number) {
+  Card card1{Color::kBlue, CardNumber::kZero};
+  EXPECT_EQ(std::get<CardNumber>(card1.getPattern()), CardNumber::kZero);
+
+  Card card2{Color::kBlue, CardNumber::kOne};
+  EXPECT_EQ(std::get<CardNumber>(card2.getPattern()), CardNumber::kOne);
+
+  Card card3{Color::kBlue, CardNumber::kTwo};
+  EXPECT_EQ(std::get<CardNumber>(card3.getPattern()), CardNumber::kTwo);
+
+  Card card4{Color::kBlue, CardNumber::kThree};
+  EXPECT_EQ(std::get<CardNumber>(card4.getPattern()), CardNumber::kThree);
+
+  Card card5{Color::kBlue, CardNumber::kFour};
+  EXPECT_EQ(std::get<CardNumber>(card5.getPattern()), CardNumber::kFour);
+
+  Card card6{Color::kBlue, CardNumber::kFive};
+  EXPECT_EQ(std::get<CardNumber>(card6.getPattern()), CardNumber::kFive);
+
+  Card card7{Color::kBlue, CardNumber::kSix};
+  EXPECT_EQ(std::get<CardNumber>(card7.getPattern()), CardNumber::kSix);
+
+  Card card8{Color::kBlue, CardNumber::kSeven};
+  EXPECT_EQ(std::get<CardNumber>(card8.getPattern()), CardNumber::kSeven);
+
+  Card card9{Color::kBlue, CardNumber::kEight};
+  EXPECT_EQ(std::get<CardNumber>(card9.getPattern()), CardNumber::kEight);
+
+  Card card10{Color::kBlue, CardNumber::kNine};
+  EXPECT_EQ(std::get<CardNumber>(card10.getPattern()), CardNumber::kNine);
+}
+
+TEST(GetPatternTest, Action) {
+  Card card1{Color::kBlue, CardAction::kDrawTwo};
+  EXPECT_EQ(std::get<CardAction>(card1.getPattern()), CardAction::kDrawTwo);
+
+  Card card2{Color::kBlue, CardAction::kReverse};
+  EXPECT_EQ(std::get<CardAction>(card2.getPattern()), CardAction::kReverse);
+
+  Card card3{Color::kBlue, CardAction::kSkip};
+  EXPECT_EQ(std::get<CardAction>(card3.getPattern()), CardAction::kSkip);
+
+  Card card4{Color::kWild, CardAction::kWild};
+  EXPECT_EQ(std::get<CardAction>(card4.getPattern()), CardAction::kWild);
+
+  Card card5{Color::kWild, CardAction::kWildDraw4};
+  EXPECT_EQ(std::get<CardAction>(card5.getPattern()), CardAction::kWildDraw4);
+
+  Card card6{Color::kWild, CardAction::kWildShuffleHands};
+  EXPECT_EQ(std::get<CardAction>(card6.getPattern()), CardAction::kWildShuffleHands);
+
+  Card card7{Color::kWild, CardAction::kWildCustomizable};
+  EXPECT_EQ(std::get<CardAction>(card7.getPattern()), CardAction::kWildCustomizable);
+}
+
+TEST(GetPatternTest, Null) {
+  Card card{};
+  EXPECT_TRUE(card.getPattern().valueless_by_exception());
+}
+
+TEST(IsEmptyTest, True) {
+  Card card{};
+  EXPECT_EQ(card.isEmpty(), true);
+}
+
+TEST(IsEmptyTest, False) {
+  Card card{Color::kBlue, CardNumber::kZero};
+  EXPECT_EQ(card.isEmpty(), false);
+}
+
+TEST(EqualTest, True) {
+  Card card1{Color::kBlue, CardNumber::kZero};
+  Card card2{Color::kBlue, CardNumber::kZero};
+  EXPECT_TRUE(card1 == card2);
+
+  Card card3{Color::kBlue, CardAction::kDrawTwo};
+  Card card4{Color::kBlue, CardAction::kDrawTwo};
+  EXPECT_TRUE(card3 == card4);
+}
+
+TEST(EqualTest, False) {
+  Card card1{Color::kBlue, CardNumber::kZero};
+  Card card2{Color::kGreen, CardNumber::kZero};
+  EXPECT_FALSE(card1 == card2);
+
+  Card card3{Color::kBlue, CardAction::kDrawTwo};
+  Card card4{Color::kBlue, CardAction::kReverse};
+  EXPECT_FALSE(card3 == card4);
+
+  Card card5{Color::kBlue, CardNumber::kZero};
+  Card card6{Color::kBlue, CardAction::kReverse};
+  EXPECT_FALSE(card5 == card6);
+}
+
+TEST(NotEqualTest, True) {
+  Card card1{Color::kBlue, CardNumber::kZero};
+  Card card2{Color::kGreen, CardNumber::kZero};
+  EXPECT_TRUE(card1 != card2);
+
+  Card card3{Color::kBlue, CardAction::kDrawTwo};
+  Card card4{Color::kBlue, CardAction::kReverse};
+  EXPECT_TRUE(card3 != card4);
+
+  Card card5{Color::kBlue, CardNumber::kZero};
+  Card card6{Color::kBlue, CardAction::kReverse};
+  EXPECT_TRUE(card5 != card6);
+}
+
+TEST(NotEqualTest, False) {
+  Card card1{Color::kBlue, CardNumber::kZero};
+  Card card2{Color::kBlue, CardNumber::kZero};
+  EXPECT_FALSE(card1 != card2);
+
+  Card card3{Color::kBlue, CardAction::kDrawTwo};
+  Card card4{Color::kBlue, CardAction::kDrawTwo};
+  EXPECT_FALSE(card3 != card4);
+}
---
+++ test/submission_test.cpp
@@ -0,0 +1,50 @@
+#include <gtest/gtest.h>
+
+#include "../src/submission.hpp"
+
+TEST(GetCardTest, Normal) {
+  Card card{Color::kBlue, CardNumber::kZero};
+  Submission submission{card, false};
+  EXPECT_EQ(submission.getCard(), card);
+}
+
+TEST(GetShouldYellUNOTest, True) {
+  Submission submission{{Color::kBlue, CardNumber::kZero}, true};
+  EXPECT_TRUE(submission.getShouldYellUNO());
+}
+
+TEST(GetShouldYellUNOTest, False) {
+  Submission submission{{Color::kBlue, CardNumber::kZero}, false};
+  EXPECT_FALSE(submission.getShouldYellUNO());
+}
+
+TEST(IsLegalTest, True) {
+  Submission submission1{{}, false};
+  EXPECT_TRUE(submission1.isLegal(Color::kBlue, CardNumber::kZero));
+
+  Submission submission2{{Color::kWild, CardAction::kWild}, false};
+  EXPECT_TRUE(submission2.isLegal(Color::kBlue, CardNumber::kZero));
+
+  Submission submission3{{Color::kBlue, CardNumber::kZero}, false};
+  EXPECT_TRUE(submission3.isLegal(Color::kBlue, CardNumber::kZero));
+
+  Submission submission4{{Color::kGreen, CardNumber::kZero}, false};
+  EXPECT_TRUE(submission4.isLegal(Color::kBlue, CardNumber::kZero));
+
+  Submission submission5{{Color::kBlue, CardNumber::kZero}, false};
+  EXPECT_TRUE(submission5.isLegal(Color::kBlue, CardNumber::kOne));
+
+  Submission submission6{{Color::kBlue, CardNumber::kZero}, false};
+  EXPECT_TRUE(submission6.isLegal(Color::kBlue, CardAction::kDrawTwo));
+}
+
+TEST(IsLegalTest, False) {
+  Submission submission1{{Color::kBlue, CardNumber::kZero}, false};
+  EXPECT_FALSE(submission1.isLegal(Color::kGreen, CardNumber::kOne));
+
+  Submission submission2{{Color::kBlue, CardAction::kDrawTwo}, false};
+  EXPECT_FALSE(submission2.isLegal(Color::kGreen, CardAction::kReverse));
+
+  Submission submission3{{Color::kBlue, CardNumber::kZero}, false};
+  EXPECT_FALSE(submission3.isLegal(Color::kGreen, CardAction::kReverse));
+}
