a7335fa96c0b86d71926d496115e06fac5bebd81
coldTOUFU <coldtoufu.dev@gmail.com>
2022-11-17 18:16:21 +0900

    fix: メソッドをいくつかconstにした。

--- src/submission.hpp
+++ src/submission.hpp
@@ -8,11 +8,11 @@ class Submission {
  public:
   Submission(Card c, bool s) : card_(c), should_yell_UNO_(s) {}
 
-  constexpr Card getCard() { return card_; }
-  constexpr bool getShouldYellUNO() { return should_yell_UNO_; }
+  constexpr Card getCard() const { return card_; }
+  constexpr bool getShouldYellUNO() const { return should_yell_UNO_; }
 
   /* この着手が合法か？ワイルドドロー4を反則で出す場合も合法として扱う。 */
-  bool isLegal(Color table_color, CardPattern table_pattern) {
+  bool isLegal(Color table_color, CardPattern table_pattern) const {
     if (card_.getColor() == Color::kNull || card_.getColor() == Color::kWild) { return true; }
 
     return card_.getColor() == table_color && card_.getPattern() == table_pattern;
