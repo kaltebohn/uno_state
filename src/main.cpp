76ed9c1acb1461712aa8bdbd2e760dec613d62cf
coldTOUFU <coldtoufu.dev@gmail.com>
2022-12-05 14:07:21 +0900

    change: 各ラウンドの得点と合計得点の両方を出力する。

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 int main(void) {
   std::array<Schlange, 4> players{};
-
+  std::array<int, 4> sum_of_score{};
   for (int i = 0; i < 1000; i++) {
     UnoStateBind2 state{allCards()};
     while (!state.isFinished()) {
@@ -12,8 +12,13 @@ int main(void) {
       player.setMyCards(state.getPlayerCards(current_player));
       state = state.next(player.move(state.legalMoves()));
     }
+    sum_of_score.at(0) += state.getScore(0);
+    sum_of_score.at(1) += state.getScore(1);
+    sum_of_score.at(2) += state.getScore(2);
+    sum_of_score.at(3) += state.getScore(3);
     std::cout << state.getScore(0) << "\t" << state.getScore(1) << "\t" << state.getScore(2) << "\t" << state.getScore(3) << std::endl;
   }
+  std::cout << sum_of_score.at(0) << "\t" << sum_of_score.at(1) << "\t" << sum_of_score.at(2) << "\t" << sum_of_score.at(3) << std::endl;
 
   return 0;
 }
